0b8192d9f7d9cf84b9549d5fa9e641ab16695ab5
835269233 <835269233@qq.com>
2025-11-16 17:13:47 +0800

    drag start coords

--- src/popup_window.rs
+++ src/popup_window.rs
@@ -1,5 +1,6 @@
 // src/popup_window.rs
 use gtk::prelude::*;
+use glib;
 use tracing::info;
 pub struct PopupWindow {
     window: gtk::Window,
@@ -41,8 +42,8 @@ impl PopupWindow {
         window.style_context().add_class("popup-window");
         let label = gtk::Label::new(None);
         label.set_line_wrap(true);
-        label.set_max_width_chars(40);
-        label.set_margin(10);
+        label.set_max_width_chars(100);
+        label.set_margin(0);
 
         // 创建垂直布局容器
         let vbox = gtk::Box::new(gtk::Orientation::Vertical, 5);
@@ -69,6 +70,56 @@ impl PopupWindow {
 
         window.add(&vbox);
 
+
+        // 添加窗口拖拽功能
+        let drag_start_coords: std::rc::Rc<std::cell::RefCell<Option<(f64, f64)>>> = 
+            std::rc::Rc::new(std::cell::RefCell::new(None));
+        let drag_start_coords_clone = drag_start_coords.clone();
+        let window_clone = window.clone();
+        
+        // 鼠标按下事件：记录起始坐标
+        window.connect_button_press_event(move |_, event| {
+            if event.button() == 1 { // 左键
+                let coords = event.root();
+                *drag_start_coords_clone.borrow_mut() = Some(coords);
+            };
+           glib::Propagation::Proceed
+        });
+        
+        let drag_start_coords_clone2 = drag_start_coords.clone();
+        let window_clone2 = window.clone();
+        
+        // 鼠标移动事件：更新窗口位置
+        window.connect_motion_notify_event(move |window, event| {
+            if let Ok(mut coords_guard) = drag_start_coords_clone2.try_borrow_mut() {
+                if let Some(start_coords) = *coords_guard {
+                    let (start_x, start_y) = start_coords;
+                    let (current_x, current_y) = event.root();
+                    
+                    // 计算窗口应该移动到的新位置
+                    let new_x = (window.position().0 as f64 + (current_x - start_x)) as i32;
+                    let new_y = (window.position().1 as f64 + (current_y - start_y)) as i32;
+                    
+                    window.move_(new_x, new_y);
+                    
+                    // 更新起始坐标，为下一次移动做准备
+                    *coords_guard = Some((current_x, current_y));
+                }
+            }
+            glib::Propagation::Proceed
+        });
+        
+        let drag_start_coords_clone3 = drag_start_coords.clone();
+        
+        // 鼠标释放事件：结束拖拽
+        window.connect_button_release_event(move |_, _| {
+            if let Ok(mut coords_guard) = drag_start_coords_clone3.try_borrow_mut() {
+                *coords_guard = None;
+            }
+            glib::Propagation::Proceed
+        });
+
+
         window.show_all();
         window.hide();
 
@@ -90,10 +141,10 @@ impl PopupWindow {
 
     pub fn show_at_mouse(&self, text: &str, x: i32, y: i32) {
         self.label.set_text(text);
-        
-        let pango_layout = self.label.create_pango_layout(Some(text));
-        let (width, height) = pango_layout.pixel_size();
-
+        let (width, height) = self.window.size_request();
+        // 修复GTK调整窗口大小的错误，确保宽度和高度大于0
+        let width = if width > 0 { width } else { 300 };
+        let height = if height > 0 { height } else { 100 };
         self.window.resize(width, height);
         self.window.move_(x, y);
         self.window.show_all();
